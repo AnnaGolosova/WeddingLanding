03fb160972275db0d024951f635b9d06998562f9
Egor Rozhkov <res23.official@gmail.com>
2025-03-17 22:07:37 +0300

    Second version

--- script.js
+++ script.js
@@ -1,36 +1,134 @@
 document.addEventListener("DOMContentLoaded", function () {
   const firstName = "Егор";
-  const secondName = "Полина";
-  // Получаем элементы
+  const secondName = "Gjkbyf";
+  var names = "";
+  var staticDate = document.getElementById("static-date");
+  const countdownBlock = document.getElementById("countdown");
+  var toggleBtn = document.getElementById("toggle-countdown");
+  let isCountdownVisible = false;
+
+  toggleBtn.addEventListener("click", function () {
+    isCountdownVisible = !isCountdownVisible;
+
+    if (isCountdownVisible) {
+      staticDate.style.display = "none";
+      countdownBlock.style.display = "block";
+      toggleBtn.innerText = "date_range"; // Иконка календаря
+      toggleBtn.style.marginTop = "10px";
+      toggleBtn.style.marginRight = "60px";
+    } else {
+      staticDate.style.display = "block";
+      countdownBlock.style.display = "none";
+
+      toggleBtn.innerText = "history"; // Иконка часов
+      toggleBtn.style.marginTop = "10px";
+      toggleBtn.style.marginRight = "5px";
+    }
+  });
+
+  function replaceElements() {
+    let gpsIcon = document.getElementById("gps"); // Родительский контейнер
+    if (!gpsIcon) return; // Если контейнера нет, выходим
+
+    if (window.innerWidth < 768) {
+      gpsIcon.remove(); // Удаляем первый элемент в коллекции (оставшиеся сдвигаются)
+    }
+  }
+
+  // Запуск при загрузке страницы
+  replaceElements();
+  // Find element and change inner.
+  if (secondName == null || secondName == "") {
+    names = `${firstName}`;
+  } else {
+    names = `${firstName} <span class="union">и</span> ${secondName}`;
+  }
+  document.getElementById("names").innerHTML = names;
+  // Get element
   const modal = document.getElementById("modal");
   const openBtn = document.getElementById("openModal");
   const closeBtn = document.querySelector(".close");
 
-  // Открываем модальное окно
+  var daysEl = document.getElementById("days");
+  var hoursEl = document.getElementById("hours");
+  var minutesEl = document.getElementById("minutes");
+  var secondsEl = document.getElementById("seconds");
+
+  // Count down timer
+  function countdownTimer() {
+    const countDownDate = new Date("06/07/2025").getTime();
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    if (distance <= 0) {
+      clearInterval(interval);
+      daysEl.innerText = "00";
+      hoursEl.innerText = "00";
+      minutesEl.innerText = "00";
+      secondsEl.innerText = "00";
+      return;
+    }
+    daysEl.innerText = formatNumber(Math.floor(distance / day));
+    hoursEl.innerText = formatNumber(Math.floor((distance % day) / hour));
+    minutesEl.innerText = formatNumber(Math.floor((distance % hour) / minute));
+    secondsEl.innerText = formatNumber(Math.floor((distance % minute) / second));
+  }
+  countdownTimer();
+  const interval = setInterval(countdownTimer, 1000);
+  function formatNumber(number) {
+    if (number < 10) {
+      return "0" + number;
+    }
+    return number;
+  }
+  countdownTimer();
+
+  // Open modal window
   openBtn.addEventListener("click", () => {
     modal.style.display = "block";
   });
 
-  // Закрываем модальное окно
+  // Close modal window
   closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
   });
 
-  // Закрываем при клике вне окна
+  // Close when to click by outside window
   window.addEventListener("click", (event) => {
     if (event.target === modal) {
       modal.style.display = "none";
     }
   });
-  // Находим элемент и меняем содержимое
-  document.getElementById("names").innerHTML = `${firstName} <span class="union">и</span> ${secondName}`;
+
   document.getElementById("allergyCheckbox").addEventListener("change", function () {
     document.getElementById("allergyInput").classList.toggle("hidden", !this.checked);
   });
 
-  // Обработчик отправки формы
+  // Form submission handler
   document.getElementById("weddingForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Отмена стандартной отправки
+    const url =
+      "https://script.google.com/macros/s/AKfycbwPo7hrWECcFQDXUUdbzoaTy3MeMsXpugtG5DLGNgEPo4mimke6UejO408HBImgaUcsFQ/exec"; // URL og Google App script
+    const formData = new FormData(this);
+
+    // Добавляем токен
+    formData.append("token", "my_secure_token_12345");
+
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        alert("Данные отправлены! Спасибо!");
+        this.reset();
+      })
+      .catch((error) => console.error("Ошибка:", error));
 
     const attend = document.getElementById("attend").checked ? "Да" : "Нет";
     const drink = document.querySelector('input[name="drink"]:checked')?.value || "Не выбрано";
